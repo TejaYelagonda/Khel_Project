133a6a29b2561c15126e5cae042dfc1a7ad94e2e
TejaYelagonda <89699684+TejaYelagonda@users.noreply.github.com>
2023-05-24 12:48:12 +0530

    Uploaded new files

---
+++ Khel_Project/Controllers/ADO_DBController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DTO;
+using System.Collections.Generic;
+
+namespace Khel_Project.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ADO_DBController : ControllerBase
+    {
+        private readonly IEmpServices  _empservices;
+        public ADO_DBController(IEmpServices empservices)
+        {
+            _empservices = empservices;
+        }
+        [HttpGet("GetEmployeeDetails")]
+        public List<EmpDTO> GetEmp()
+        {
+            var data = _empservices.GetEmp();
+            return data;
+        }
+    }
+}
